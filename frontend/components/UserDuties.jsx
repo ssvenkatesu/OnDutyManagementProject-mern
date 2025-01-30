1dbe5a8a3555d54bdeafeeb4bdc59a093924a800
ssvenkatesu <ssivasrivenkatesu31@gmail.com>
2025-01-30 12:09:36 +0530

    commit

--- frontend/components/UserDuties.jsx
+++ frontend/components/UserDuties.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import {Link} from 'react-router-dom';
 
 const handleReload = () => {
   window.location.reload();
@@ -79,6 +80,7 @@ const UserDuties = () => {
         <p>No duties assigned.</p>
       )}
       <br />
+    
       
       </div>
     
